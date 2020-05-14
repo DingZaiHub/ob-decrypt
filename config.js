953adde5c6e23824eae732780388d102402bff07
DingZaiHub <52557898+DingZaiHub@users.noreply.github.com>
2020-05-14 12:37:02 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+var config = {
+    // 要还原的代码路径
+    file2decrypt: "./source.js",
+    // 是否生成格式化后的代码，false为格式化，true为禁止格式化
+    compact: false,
+    // 要生成的代码路径
+    file2generat: "./code.js",
+}
+
+module.exports = { config }
