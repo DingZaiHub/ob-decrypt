02d79255bb261604e39c2259710fa1a9b120db8c
DingZaiHub <52557898+DingZaiHub@users.noreply.github.com>
2020-06-05 12:24:21 +0800

    增加配置选项

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 var config = {
     // 要还原的代码路径
     file2decrypt: "./source.js",
-    // 是否生成格式化后的代码，false为格式化，true为禁止格式化
-    compact: false,
     // 仅将解密函数还原，其它不还原。如仅还原_0x5323("0x1a", ")^7H")。true为仅还原解密函数
     only_decrypt: false,
     // 开启打印解密函数还原过程的开关，true为打开，false关闭
     debug: false,
+    // 常量计算，如"eAj" === "eAj"直接转为true。可能会导致逻辑错误，慎用！
+    eval: false,
+    // 修改调用方式，如aa['bb']['v']()转aa.bb.v()，但aa[']']转成aa.]代码会出错，慎用！
+    trans: false,
     // 要生成的代码路径
-    file2generat: "./code.js",
+    file2generate: "./code.js",
 }
 
 module.exports = { config }
